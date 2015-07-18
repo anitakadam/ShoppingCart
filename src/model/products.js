385871b305f1649e46be0e29d3ca4da32c6cd4a9
anitakadam <anitakadam11@gmail.com>
2015-07-19 00:56:33 +0530

    Added more products

--- src/model/products.js
+++ src/model/products.js
@@ -46,5 +46,99 @@ module.exports = [
             ],
             description: "Samsung’s GALAXY Tab E is a well-designed, high-performing tablet that maximizes performance and usage, allowing you to multi-task like a boss, since you can download office apps from the Galaxy Store.",
             price : "16990"
+        },
+        {
+            id: "3",
+            name : "Asus Zenfone 2 ZE551ML",
+            imgUrl: "http://img5a.flixcart.com/image/mobile/h/7/n/asus-ze551ml-ze551ml-6jww-400x400-imae6qashugarq3r.jpeg",
+            features: [
+                'Android v5 (Lollipop) OS',
+                '13 MP Primary Camera',
+                '5 MP Secondary Camera',
+                'Dual Sim (GSM + LTE)',
+                '5.5 inch Touchscreen',
+                '2.3 GHz Intel Atom Z3580 Quad Core Processor',
+                'Expandable Storage Capacity of 64 GB',
+                '4G (LTE) - 150 Mbps HSDPA',
+                '50 Mbps HSUPA'
+            ],
+            description: "Note: SIM Card Slot 1 can be Used to Access 3G/4G/LTE Data. SIM Card Slot 2 can Only be Used for 2G Services which Include Voice and Basic Text Messaging (Not Multimedia Messaging Services MMS or Internet Browsing)",
+            price : "29999"
+        },
+        {
+            id: "4",
+            name : "Datawind UbiSlate 7CZ Tablet(4 GB, Wi-Fi+2G)",
+            imgUrl: "http://img5a.flixcart.com/image/tablet/w/g/7/datawind-ubislate-7cz-400x400-imadzcx3xegegc8n.jpeg",
+            features: [
+                '1.3 GHz Cortex-A7 Dual Core Processor',
+                'Bluetooth and 2G Calling',
+                'Android v4.4.2 (KitKat) OS',
+                'Wi-Fi Enabled and 3G Dongle Support',
+                '7 inch TFT Capacitive Touchscreen',
+                '2 MP Primary Camera',
+                '0.3 MP Secondary Camera',
+                'Expandable Storage Capacity of 32 GB'
+            ],
+            description: "1 Year manufacturer warranty for the Tablet, 6 months manufacturer warranty for other inbox accessories",
+            price : "3199"
+        },
+        {
+            id: "5",
+            name : "Apple iPad Air 2 Wi-Fi + Cellular 128 GB Tablet",
+            imgUrl: "http://img5a.flixcart.com/image/tablet/y/a/b/apple-ipad-air-2-wi-fi-cellular-128-gb-400x400-imae2y36yx4eqygn.jpeg",
+            features: [
+                '1.2 MP Secondary Camera',
+                'A8X Chip with M8 Motion Co-processor',
+                '9.7 inch LED Touchscreen',
+                'Full HD Recording',
+                '8 MP Primary Camera',
+                'FaceTime',
+                'Wi-Fi Enabled'
+            ],
+            description: "Beautifully designed and immensely powerful, the alpha male of the tablets pack has finally arrived. The rightful successor of the iPad Air, the lust-worthy iPad Air 2 makes you fall in love with it over and over again.",
+            price : "58440"
+        },
+        {
+            id: "6",
+            name : "Dell S2240L 21.5 inch LED Backlit LCD Monitor",
+            imgUrl: "http://img6a.flixcart.com/image/monitor/y/p/r/dell-s2240l-400x400-imadf4pydtfawtgy.jpeg",
+            features: [
+                'Full HD/HD Display',
+                'Brightness: 250 nits',
+                '54.61 cm LED Backlit LCD Display',
+                '1000:1 Contrast Ratio',
+                'IPS Panel Type',
+                '1920 x 1080 pixel Resolution'
+            ],
+            description: "Virtually Borderless, Ultrawide Viewing Angle, Stunning Clarity, Customizable Color, Stay Versatile, Save Resources, Environment Responsible - Energy Star",
+            price : "58440"
+        },
+        {
+            id: "7",
+            name : "Apple MD711HN/B MacBook Air (Ci5/ 4GB/ 128GB Flash/ Mac OS X Mavericks)",
+            imgUrl: "http://img5a.flixcart.com/image/computer/r/z/g/apple-macbook-air-netbook-400x400-imadwdzsqzuktbz6.jpeg",
+            features: [
+                'Intel Core i5',
+                '4 GB DDR3 RAM',
+                '128 GB SSD',
+                'Mac OS X Mavericks'
+            ],
+            description: "It's slim, beautiful and mind-blowing. We're talking about the Macbook Air. Take it with you wherever you go, watch movies, get your work done and chat with your loved ones too, because it's perfect in every way.",
+            price : "54500"
+        },
+        {
+            id: "8",
+            name : "Nikon D5300 (Body with AF-S DX NIKKOR 18-55 mm f/3.5-5.6G VR II) DSLR Camera",
+            imgUrl: "http://img5a.flixcart.com/image/camera/g/4/k/nikon-d5300-dslr-400x400-imaeyxfauurgnpzk.jpeg",
+            features: [
+                '24.2 Megapixel Camera',
+                'Full HD Recording',
+                'ISO 100 - ISO 12800 Sensitivity',
+                'CMOS Image Sensor',
+                '3.2 inch Vari-angle TFT Monitor with 170° Viewing Angle'
+            ],
+            description: "Nikon presents the D5300 camera that comes with an AF-S 18-55mm VR lens - a DSLR crafted to be lightweight yet full on functionality, helping you revamp your photographic skills.",
+            price : "42457"
         }
-     ];
+
+];
