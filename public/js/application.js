e0f8429646bca9c6ffeaecc6fef6525552cdbacb
pradiphudekar <pradiphudekar@gmail.com>
2015-07-19 13:12:40 +0530

    adding items to cart

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,18 @@
 var app = angular.module("ShoppingCart", [])
 
-app.controller("ShopController", function($http) {
+app.controller("ShopController", function ($http) {
     var self = this;
     this.products = [];
+    this.cartItems = [];
     $http.get("/api/products")
-        .then(function(response){
-            self.products = response.data;
-        });
+        .then(function (response) {
+        self.products = response.data;
+    });
+
+    this.addToCart = function (product) {
+        this.cartItems.push(product);
+    }
+
 });
 
 
