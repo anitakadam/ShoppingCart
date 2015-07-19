f9f344cb164816367073840ebdfbde76802d1100
pradiphudekar <pradiphudekar@gmail.com>
2015-07-19 13:36:36 +0530

    using routeProvider

--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,39 @@
-var app = angular.module("ShoppingCart", [])
+var app = angular.module("ShoppingCart", ["ngRoute"])
 
-app.controller("ShopController", function ($http) {
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "views/product-list.html",
+        controller: "ShopController as shop"
+    }).when("/cart", {
+        templateUrl: "views/cart.html",
+        controller: "CartController as cart"
+    });
+});
+
+app.service("cartService", function () {
+    this.cartItems = [];
+    this.addProduct = function (product) {
+        this.cartItems.push(product);
+    };
+});
+
+app.controller("ShopController", function ($http, cartService) {
     var self = this;
     this.products = [];
-    this.cartItems = [];
+    this.cartItems = cartService.cartItems;
+
     $http.get("/api/products")
         .then(function (response) {
         self.products = response.data;
     });
 
     this.addToCart = function (product) {
-        this.cartItems.push(product);
-    }
+        cartService.addProduct(product);
+    };
+});
 
+app.controller("CartController", function (cartService) {
+    this.cartItems = cartService.cartItems;
 });
 
 
